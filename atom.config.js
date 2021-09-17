45d3ea17822ab3f2c8e306948da2510844578a0e
zhaoxiaohan03 <zhaoxiaohan03@kuaishou.com>
2021-09-17 20:40:50 +0800

    feat: 添加配置文件

---
+++ atom.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    webDomian: 'https://github.com/zxhnext/atom-global-banner.git',
+    gitUrl: 'git@github.com:zxhnext/atom-global-banner.git',
+    name: 'atom-global-banner',
+    description: "远程banner组件"
+  }
